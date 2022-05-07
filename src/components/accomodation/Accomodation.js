fd30fc2aab67f686e224e3d212145ad1660114f0
Tina Dahl <tina@e5r.no>
2022-05-07 17:11:54 +0200

    Add accommodation page

--- src/components/accomodation/Accomodation.js
+++ src/components/accomodation/Accomodation.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import Container from "react-bootstrap/Container";
+import Footer from "../layout/Footer";
+import Heading from "../typography/Heading";
+import AccomodationList from "./AccommodationList";
 
 export default function Accomodation() {
   return (
-    <div>Accomodation</div>
+    <>
+      <Container className="my-5">
+        <Heading Tag="h1" title="Accommodation" />
+        <AccomodationList />
+      </Container>
+      <Footer />
+    </>
   )
 }
