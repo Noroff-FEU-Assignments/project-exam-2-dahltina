c32142f546b153b8d1d43cd9b39f198e99405162
Tina Dahl <tina@e5r.no>
2022-05-20 10:47:33 +0200

    Fix add accommodation form

--- src/components/forms/AddAccommodationForm.js
+++ src/components/forms/AddAccommodationForm.js
@@ -1,15 +1,12 @@
-import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
-// import ValidationError from "./ValidationError";
-// import { useNavigate } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Alert from "react-bootstrap/Alert";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Heading from "../typography/Heading";
+import ValidationError from "./ValidationError";
 import useAxios from "../../hooks/useAxios";
 
 const schema = yup.object().shape({
@@ -18,115 +15,91 @@ const schema = yup.object().shape({
   price: yup.number().required("Please enter price per night"),
   facilities: yup.string().required("Please enter facilities"),
   excerpt: yup.string().required("Please enter a short description"),
-  description: yup.string().required("Please enter description")
+  description: yup.string().required("Please enter full description")
 })
 
-export default function AddAccommodationForm() {
+export default function BookingForm() {
 
   const [submitting, setSubmitting] = useState(false);
   const [submitted, setSubmitted] = useState(false);
   const [serverError, setServerError] = useState(null);
+  const { register, handleSubmit, formState: { errors }} = useForm({ resolver: yupResolver(schema) });
   const http = useAxios();
 
-  // const navigate = useNavigate();
-  // const LinkToNewItem = () => {
-  //   navigate("/accommodation/id");
-  // };
-
-  const { register, handleSubmit, errors } = useForm({
-    resolver: yupResolver(schema)
-  });
-
   async function onSubmit(data) {
-    setSubmitting(true);
-    setServerError(null);
-    data.status = "publish";
+      console.log(data);
+      setSubmitting(true);
+      setServerError(null);
 
-    try {
-      const response = await http.post("wp/v2/accommodations/", data);
-      console.log("response", response.data);
-    }
+      const newData = {
+        status: "publish",
+        fields: data,
+      }
 
-    catch (error) {
-      console.log(error);
-      setServerError(error.toString());
-    }
+      try {
+        const response = await http.post("wp/v2/accommodations", newData);
+        console.log("response:", response.data);
 
-    finally {
-      setSubmitting(false);
-      setSubmitted(true);
-    }
+        if (response.ok) {
+          setSubmitted(true);
+        }
+      }
+
+      catch (error) {
+        console.log("error", error);
+        setServerError(error.toString());
+      }
+
+      finally {
+        setSubmitting(false);
+      }
   }
+
+  console.log(errors);
 
   return (
     <>
       <Form id="add-accommodation-form" onSubmit={handleSubmit(onSubmit)}>
-        <Heading Tag="h1" title="Add accommodation" className="text-center mb-4"/>
-        {serverError && <Alert className="alert-danger text-center">{serverError}</Alert>}
+        <Heading Tag="h3" title="Add accommodation info" className="mb-3 text-center"/>
         {submitted && <Alert variant="success"><h4>Success</h4> New accommodation has been added.</Alert>}
+        {serverError && <Alert className="alert-danger text-center">{serverError}</Alert>}
+
         <fieldset disabled={submitting}>
+          <Form.Group className="mb-3" >
+            <Form.Control type="text" placeholder="Title" {...register("title")} />
+            {errors.title && <ValidationError>{errors.title.message}</ValidationError>}
+          </Form.Group>
 
-          <Row>
-            <Col className="col-12 col-md-6">
-              <Form.Group className="mb-3" >
-                <Form.Control type="text" placeholder="Title" {...register("title")} />
-                {/* {errors.title && <ValidationError>{errors.title.message}</ValidationError>} */}
-              </Form.Group>
+          <Form.Group className="mb-3" >
+            <Form.Control type="text" placeholder="Location" {...register("location")} />
+            {errors.location && <ValidationError>{errors.location.message}</ValidationError>}
+          </Form.Group>
 
-              <Form.Group className="mb-3" >
-                <Form.Control type="text" placeholder="Location" {...register("fields.location")} />
-                {/* {errors.location && <ValidationError>{errors.location.message}</ValidationError>} */}
-              </Form.Group>
+          <Form.Group className="mb-3" >
+            <Form.Control type="text" placeholder="Price" {...register("price")} />
+            {errors.price && <ValidationError>{errors.price.message}</ValidationError>}
+          </Form.Group>
 
-              <Form.Group className="mb-3" >
-                <Form.Control type="text" placeholder="Price" {...register("fields.price")} />
-                {/* {errors.price && <ValidationError>{errors.price.message}</ValidationError>} */}
-              </Form.Group>
+          <Form.Group className="mb-3" >
+            <Form.Control type="text" placeholder="Facilities" {...register("facilities")} />
+            {errors.facilities && <ValidationError>{errors.facilities.message}</ValidationError>}
+          </Form.Group>
 
-              <Form.Group className="mb-3" >
-                <Form.Control type="text" placeholder="Facilities" {...register("fields.facilities")} />
-              </Form.Group>
+          <Form.Group className="mb-3" >
+            <Form.Control as="textarea" rows={2} placeholder="Excerpt" {...register("excerpt")}/>
+            {errors.excerpt && <ValidationError>{errors.excerpt.message}</ValidationError>}
+          </Form.Group>
 
-              <Form.Group className="mb-3" >
-                <Form.Control as="textarea" rows={2} placeholder="Excerpt" {...register("excerpt")}/>
-              </Form.Group>
+          <Form.Group className="mb-3" >
+            <Form.Control as="textarea" rows={3} placeholder="Description" {...register("description")}/>
+            {errors.description && <ValidationError>{errors.description.message}</ValidationError>}
+          </Form.Group>
 
-              <Form.Group className="mb-3" >
-                <Form.Control as="textarea" rows={3} placeholder="Description" {...register("description")}/>
-              </Form.Group>
-
-            </Col>
-            <Col className="col-12 col-md-6">
-
-              <Form.Group className="mb-3" >
-                <Form.Label>Main image (Displayed on home page. Must be square format)</Form.Label>
-                <Form.Control type="file" {...register("fields.img_thumb")} />
-              </Form.Group>
-
-              <Form.Group className="mb-3" >
-                <Form.Label>Image 1 (Should be 2000 x 1333px)</Form.Label>
-                <Form.Control type="file" {...register("fields.img_1")} />
-              </Form.Group>
-
-              <Form.Group className="mb-3" >
-                <Form.Label>Image 2 (Should be 2000 x 1333px)</Form.Label>
-                <Form.Control type="file" {...register("fields.img_2")} />
-              </Form.Group>
-
-              <Form.Group className="mb-3" >
-                <Form.Label>Image 3 (Should be 2000 x 1333px)</Form.Label>
-                <Form.Control type="file" {...register("fields.img_3")} />
-              </Form.Group>
-
-              <Button variant="primary" type="submit" >
-                {submitting ? "Adding..." : "Add accommodation"}
-              </Button>
-
-            </Col>
-          </Row>
+          <Button variant="primary" type="submit" >
+            {submitting ? "Adding..." : "Add accommodation"}
+          </Button>
         </fieldset>
       </Form>
     </>
   )
 }
-
